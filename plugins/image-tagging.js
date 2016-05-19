8ef76221365cb1556905eba73821e013c5aef490
Thomas Wall <thomaswall@thomass-mbp.usny.ibm.com>
2016-05-19 17:48:59 -0400

    console cleanup

--- plugins/image-tagging.js
+++ plugins/image-tagging.js
@@ -2,7 +2,7 @@ const onMessage = message => {
 
     if(!message.imageTags || !message.imageTags.length || message.imageTags[0].text == 'NO_TAGS')
         return Promise.resolve(false);
-    console.log(message.imageTags[0].text)
+
     return Promise.resolve(`That looks like a ${message.imageTags[0].text}!`)
 
 }
