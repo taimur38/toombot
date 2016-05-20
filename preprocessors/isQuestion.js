77c1de6f40edd4970f941d188840303d63628c90
Taimur Shah <shaht@us.ibm.com>
2016-05-20 18:51:10 -0400

    shitty detect

---
+++ preprocessors/isQuestion.js
@@ -0,0 +1,11 @@
+const Process = message => {
+
+	return Promise.resolve(
+		{ isQuestion: (message.text.startsWith("what") || message.text.startsWith("how") || message.text.indexOf("?") >= 0) }
+	);
+
+}
+
+module.exports = {
+	Process
+}
