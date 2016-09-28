a51fe66c669169b7141713f526d8694dba7351c0
Taimur Shah <taimur38@gmail.com>
2016-09-27 23:56:40 -0400

    cleanup

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
 const MemoryDataStore = require('@slack/client').MemoryDataStore;
 const Rx = require('rx');
 
-const cogenv = require('cogenv-lib');
-
 const preprocessor = require('./preprocessors');
 const plugins = require('./plugins');
 
@@ -16,15 +14,6 @@ const rtm = new RtmClient(token, {
 });
 
 rtm.start();
-
-const cogEnvConfig = {
-	exchange: 'globe',
-	exchangeType: 'topic',
-	source: "toombot",
-	host: 'amqp://watson:watsonpass@austin.watson-proto.blue'
-};
-
-const room = new cogenv(cogEnvConfig);
 
 const slackClean = message => {
 	return Object.assign({}, message, {
@@ -43,20 +32,10 @@ const processed = message_source
 	.filter(message => message.type == 'message' && !message.subtype)
 	.map(slackClean)
 	.filter(message => message.user.name != 'toombot')
-	.flatMap(message => Rx.Observable.fromPromise(preprocessor(message)))
+	.flatMap(message => preprocessor(message))
 	.tap(console.log)
-	.tap(message => {
-		if(message.channel.name != 'area51')
-			return;
 
-		if(message.links) {
-			for(let i = 0; i < message.links.length; i++)
-				room.broadcast("passive.display", [{ url: message.links[i].url }] )
-		}
-	})
-
-
-processed.flatMap(message => Rx.Observable.fromPromise(Promise.all(plugins.map(p => p(message)))))
+processed.flatMap(message => Promise.all(plugins.map(p => p(message))))
 	.flatMap(r => { console.log(r); return r; }) // flattens array
 	.filter(r => r && r.content)
 	.subscribe(r => {
