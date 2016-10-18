5892b05a7da23e317c03635d71cfd24753db5357
Taimur Shah <shaht@us.ibm.com>
2016-10-18 16:25:07 -0400

    length minimum on questions

--- preprocessors/isQuestion.js
+++ preprocessors/isQuestion.js
@@ -2,10 +2,12 @@ const Process = message => {
 
 	return Promise.resolve(
 		{ isQuestion: (
-			message.text.startsWith("what") ||
-			message.text.startsWith("how") ||
-			message.text.startsWith("who") ||
-			message.text.indexOf("?") >= 0) }
+			message.text.split(' ').length >= 3 && (
+				message.text.startsWith("what") ||
+				message.text.startsWith("how") ||
+				message.text.startsWith("who") ||
+				message.text.indexOf("?") >= 0) }
+			)
 	);
 
 }
