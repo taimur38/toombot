1d4c84e6f752d71db985cf3100eda2984d84190a
Taimur Shah <taimur38@gmail.com>
2016-12-29 12:58:00 -0500

    ask about images first

--- minions/image-commenter.ts
+++ minions/image-commenter.ts
@@ -26,14 +26,29 @@ function* onMessage(message : SlackMessage & imagize.Response & linkMeta.Respons
 
     console.log(JSON.stringify(message.imageTags[0].classes, undefined, 2))
 
-    const query = message.imageTags[0].classes
+    const topClasses = message.imageTags[0].classes
         .map(c => ({ ...c, super_score: c.type_hierarchy == undefined ? c.score : c.score + c.type_hierarchy.split('/').length/3 }))
         .filter(c => c.super_score > 0.8 && c.class.indexOf('color') == -1)
         .sort((a, b) => b.super_score - a.super_score)
         .slice(0, 2)
+    
+// testingg
+    const response : SlackMessage = yield Promise.resolve({
+        send: true,
+        text: "is this picture about " + topClasses.map(c => c.class).join(' and ') + "?",
+        contextMatch: (msg : SlackMessage) => msg.channel.id == message.channel.id,
+        filter: (msg : SlackMessage) => msg.text.search(/yeah|yup|yep|yes|no|nope/gi) > -1
+    }) 
+
+    if(response.text.search(/no|nope/gi) > -1)
+        return Promise.resolve({ 
+            text: ":(",
+            send: true
+        })
+
+    const query = topClasses
         .reduce((agg, curr) => agg + '(' + curr.class.split(' ').join(' AND ') + ') OR ', '')
 
-    
     console.log(query);
     return reddit_session.get(`/search.json?q=${query}`)
         .then(rsp => {
@@ -42,16 +57,16 @@ function* onMessage(message : SlackMessage & imagize.Response & linkMeta.Respons
             throw new Error('no results')
         })
         .then(posts => {
-            const post = posts[0]
+            const post = posts[0].data;
             if(post.is_self)
                 return {
                     send: true,
-                    text: post.data.selftext + ': ' + post.data.url
+                    text: post.selftext + ': ' + post.url
                 }
             else
                 return {
                     send: true,
-                    text: post.data.title + ': ' + post.data.url
+                    text: post.title + ': ' + post.url
                 }
         })
 	.catch(err => { console.error(err); return undefined } )
