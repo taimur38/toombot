4219de98e20d636eea06bd1e1264f7f93c6b4c41
Taimur Shah <taimur38@gmail.com>
2017-03-27 20:16:24 -0400

    etf support

--- minions/stocks.ts
+++ minions/stocks.ts
@@ -9,9 +9,9 @@ function* onMessage(message : SlackMessage) : Iterator<Promise<MinionResult>> {
     const symbol = match[1];
     console.log(symbol)
 
-    const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?formatted=true&crumb=lnRQn70gX0Q&lang=en-US&region=US&modules=summaryProfile%2CfinancialData%2CrecommendationTrend%2CupgradeDowngradeHistory%2Cearnings%2CdefaultKeyStatistics%2CcalendarEvents`
+    const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${symbol}?formatted=true&crumb=lnRQn70gX0Q&lang=en-US&region=US&modules=summaryProfile,financialData,recommendationTrend,upgradeDowngradeHistory,earnings,defaultKeyStatistics,calendarEvents,assetProfile,topHoldings,fundPerformance,fundProfile`;
 
-    const chart_url = `https://query2.finance.yahoo.com/v7/finance/chart/${symbol}?range=1mo&interval=5d&indicators=quote&includeTimestamps=true&includePrePost=false`
+    const chart_url = `https://query2.finance.yahoo.com/v7/finance/chart/${symbol}?range=1mo&interval=1d&indicators=quote&includeTimestamps=true&includePrePost=false`
 
     return Promise.all([axios.get(url), axios.get(chart_url)])
         .then(res => ({ financials: res[0].data as any, chart: res[1].data as any }))
@@ -19,10 +19,15 @@ function* onMessage(message : SlackMessage) : Iterator<Promise<MinionResult>> {
             //console.log(data.quoteSummary.result);
             const result = compoundResult.financials.quoteSummary.result[0];
 
+            const isETF = result.fundPerformance !== undefined;
+
             const diff_indicators = compoundResult.chart.chart.result[0].indicators;
 
-            const initialClose = diff_indicators.quote[0].close[0];
-            const currentPrice = result.financialData.currentPrice.raw;
+            const closes = diff_indicators.quote[0].close;
+            const opens = diff_indicators.quote[0].open;
+            const initialClose= closes[0];
+            //const currentPrice = isETF ? result.result.financialData.currentPrice.raw;
+            const currentPrice = closes[closes.length - 1];
 
             let percent = (currentPrice - initialClose)/initialClose * 100;
             let descriptor = 'up';
@@ -31,9 +36,21 @@ function* onMessage(message : SlackMessage) : Iterator<Promise<MinionResult>> {
                 percent = -1 * percent;
             }
 
+            let dayDescrip = 'up'
+            let dayPercent = (currentPrice - opens[opens.length - 1])/opens[opens.length - 1] * 100;
+            if(dayPercent < 0) {
+                dayDescrip = 'down'
+                dayPercent = -1 * dayPercent;
+            }
+
+            const base = `current price of ${symbol} is ${currentPrice.toFixed(2)}.\n${dayDescrip} ${dayPercent.toFixed(2)}% today.\n${descriptor} ${percent.toFixed(2)}% this month.`
+            if(isETF){
+                return {
+                    text: base
+                }
+            }
             return { 
-                text: `current price of ${symbol} is ${currentPrice} - ${descriptor} ${percent.toFixed(2)}% this month. \n Analyst recommendation is to ${result.financialData.recommendationKey}`,
-                threadReply: true
+                text: `${base}\n Analyst recommendation is to ${result.financialData.recommendationKey}`
             }
         })
         .catch(err => {
