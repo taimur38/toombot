eddbd96f8b828e88da2a9092afa5afa5d8003a3a
Taimur Shah <taimur38@gmail.com>
2017-03-30 21:42:08 -0400

    use the awaits

--- ingest/quotables.ts
+++ ingest/quotables.ts
@@ -18,40 +18,48 @@ async function annotate(message : string, author: string) {
     MERGE (q)-[:HAS_AUTHOR]->(a)
   `;
 
-  await session.run(transaction).catch((err : Error) => console.error('quote ingest error', err));
-
-  await alchemy.getAllTheThings(message)
-		.then(things => ({ alchemy: things, id: id}))
-    .then(res => {
-      const tx = session.beginTransaction();
-      const transactions = toombatize.annotate(res, "Quote");
-
-      for(let trans of transactions)
-        tx.run(trans).catch((err : Error) => console.error('toombatize tx run error', err));
-
-      return tx.commit()
-    		.then(() => session.close())
-    		.catch(() => session.close())
-    })
-		.catch((err : Error) => {
-			console.log("Preprocessor: " + err);
-			console.log(err);
-      session.close();
-			return;
-		})
-
+  try {
+    await session.run(transaction)
+  }
+  catch(e) {
+    console.error('couldnt ingest quote', e);
     return;
+  }
+
+  try {
+    const things = await alchemy.getAllTheThings(message);
+    const transactions = toombatize.annotate({alchemy: things, id }, "Quote");
+
+    for(let trans of transactions) {
+      try {
+        await session.run(trans)
+      } catch(e) {
+        console.error('error running statement', e);
+      }
+    }
+  } catch(e) {
+    console.error('error getting alchemy', e)
+  }
+
+  return true;
 }
+
+const whitelist = /hemmingway|nietzsche|aesop|alan watts|albert camus|albert einstein|aldous huxley|alexander hamilton|barack obama|blaise pascal|bob dylan|bob marley|buddha|bruce lee|carl sagan|che guevara|woody allen|winston churchill|warren buffett|walt disney|walt whitman|vincent van gogh|tupac shakur|steve jobs|sun tzu|steve ballmer|stephen hawking|steve irwin|socrates|soren kierkegaard|sophocles|sigmund freud|salvador dali|roald dahl|rene descartes|ram dass|ray bradbury|ralph waldo emerson|plato|pablo picasso|orson welles|orson scott card|orson scott card|niccolo machiavelli|nelson mandela|neil gaiman|napoleon bonaparte|muhammad ali|muhammad ali jinnah|muhammad iqbal|mitch hedberg|mohsin hamid|milton friedman|mike tyson|michelangelo|michel de montaigne|martin luther king|martin luther|mark twain|marcel proust|malcolm x|mahatma gandhi|leonardo da vinci|leo tolstoy|lao tzu|kurt vonnegut|kurt cobain|karl marx|julius caesar|john steinbeck|john locke|kennedy|eisenhower|rockefeller|goethe|jimi hendrix|jesus christ|sartre|tolkien|isaac newton|isaac asimov|kant|thoreau|george washington|bush|orwell|patton|cantor|galileo|dostoevsky|nietzsche|franz kafka|rouchefoucald|scott fitzgerald|ernest hemingway|cantona|epicurus|enzo ferrari|eminem|edgar allan poe|cummings|seuss|douglas adams|donald trump|david lynch|dante alighieri|dalai lama|confucius/gi
 
 async function parse() {
   fs.readFile('./ingest/author-quote.txt', 'utf8', async function (err,data) {
-    if (err) {
+    if (err) {0
       return console.log(err);
     }
     let quotes = data.split("\n");
     for(let quote of quotes) {
-      let q = quote.split("\t");
-      await annotate(q[1], q[0]);
+      const [author, text] = quote.split("\t");
+
+      if(author && author.search(whitelist) > -1) {
+        console.log(`ingesting ${author}`)
+        await annotate(text, author);
+      }
+
     }
   });
 }
--- minions/quotes.ts
+++ minions/quotes.ts
@@ -7,30 +7,46 @@ import * as context from './context'
 const neo4j = require('neo4j-driver').v1;
 const driver = neo4j.driver(`bolt://${process.env.NEO_URL}`, neo4j.auth.basic(process.env.NEO_USER, process.env.NEO_PASS))
 
+const context_threshold = 0.4;
+const query_threshold = 0.4;
+
 function* onMessage(message : SlackMessage & context.Response) : Iterator<Promise<MinionResult>> {
   return quotes(message);
 }
 
 async function quotes(response : SlackMessage & context.Response) : Promise<MinionResult> {
-	let concepts = response.context.concepts.filter(c => c.relevance > 0.4).sort((a,b) => b.relevance - a.relevance)
-	let entities = response.context.entities.filter(c => c.relevance > 0.4).sort((a,b) => b.relevance - a.relevance);
 
-  let concept_merge = [...concepts, ...entities].sort((a,b) => b.relevance - a.relevance);
+  const concept_merge = [...response.context.concepts, ...response.context.entities].filter(c => c.relevance > context_threshold).sort((a, b) => b.relevance - a.relevance);
 
-  if(concept_merge.length < 1)
-    return Promise.resolve({ text: 'i have no quotes', send: true })
+  if(concept_merge.length == 0) {
+    return { text: 'i have no quotes', send: true }
+  }
+  
+  const concept_labels = concept_merge.map(c => `"${c.text}"`);
+  const session = driver.session();
 
-  let concept = concept_merge[0].text;
-  console.log(concept_merge[0])
-  let session = driver.session();
-
-  let transaction = `
-    MATCH (a:Author)--(q:Quote)--(c)
-    WHERE c.id="${concept}" and (c:Concept or c:Entity)
-    RETURN a.id as author,q.text as quote
+  const query = `
+    MATCH (a:Author)--(q:Quote)-[r:HAS_CONCEPT|:HAS_ENTITY]-(c)
+    WHERE toFloat(r.score) > ${query_threshold} AND c.id  in [${concept_labels}]
+    RETURN a.id as author, q.text as quote, filter(x in collect(c) where x.id IN [${concept_labels}]) as overlap 
+    order by size(overlap) desc
+    limit 10
   `;
 
-  return session.run(transaction)
+  try {
+    const results = await session.run(query);
+
+    const record = results.records[0];
+    return { text: `${record.get('quote')}\n\n*${record.get('author')}*`, send: true };
+
+  } catch(e) {
+    console.error(e)
+  } finally {
+    console.log('closing session')
+    session.close();
+  }
+
+  /*
     .then(res => {
       const record = res.records[0];
       session.close();
@@ -40,10 +56,11 @@ async function quotes(response : SlackMessage & context.Response) : Promise<Mini
       return { text: `"${record.get('quote')}" - ${record.get('author')}`, send: true };
     })
     .catch(err => session.close())
+    */
 }
 
 const mod : MinionModule = {
-	onMessage,
+  onMessage,
 	key: 'quote',
 	filter: msg => msg.text.toLowerCase().indexOf("quote") > -1,
   requirements: ['context']
