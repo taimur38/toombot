fbda300421b9e36ec67ca4041e4b3d6127de0d25
Manali Chanchlani <manalichanchlani@Manalis-MBP.pavlov.gateway>
2017-04-19 15:29:28 -0400

    clean up

--- minions/imdb.ts
+++ minions/imdb.ts
@@ -2,31 +2,25 @@ import * as axios from 'axios';
 
 import { SlackMessage, MinionResult, MinionModule } from '../types'
 
+function* onMessage(message: SlackMessage): Iterator<Promise<MinionResult>> {
 
-function* onMessage(message : SlackMessage) : Iterator<Promise<MinionResult>> {
+  let query = message.text.replace('imdb ', '');
 
-  // const config = { "headers": {
-  //   "X-Mashape-Key": "",
-  //   "Accept": "application/json"}};
-	let query = message.text.replace('imdb ', '');
-
-	// return axios.get(`https://community-netflix-roulette.p.mashape.com/api.php?title=${query}`, config)
   return axios.get(`http://www.omdbapi.com/?t=${query}`)
-		.then(res => {
+    .then(res => {
       let response = '';
-      if(res.data['Plot'])
+      if (res.data['Plot'])
         response = "Title: " + res.data['Title'] + "\n Year: " + res.data['Year'] + "\n Genre: " + res.data['Genre'] + "\n Rating: " + res.data['imdbRating'] + "\n Plot: " + res.data['Plot']
       else
         return false;
       return { text: response, send: true };
-
-		})
-		.catch(err => { console.log(err); return false; })
+    })
+    .catch(err => { console.log(err); return false; })
 }
 
-const mod : MinionModule = {
-	onMessage,
-	key: 'imdb',
+const mod: MinionModule = {
+  onMessage,
+  key: 'imdb',
   filter: msg => msg.text.toLowerCase().startsWith(`imdb`)
 }
 
